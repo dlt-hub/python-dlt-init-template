2467e172b0864cebb9348554d44b2c86d97257bb
Adrian <Adrian>
2022-11-24 13:58:52 +0100

    add twitter init

---
+++ variants/twitter.py
@@ -0,0 +1,54 @@
+import requests
+import dlt
+
+
+# a source is a container for the data-producing resources, it returns parametrized resources
+@dlt.source
+def twitter(api_url=dlt.config.value, twitter_bearer_token=dlt.secrets.value, last_id=0):
+    # example of Bearer Authentication
+    # create authorization headers
+    return twitter_resource(twitter_bearer_token)
+
+
+def _headers(twitter_bearer_token):
+    headers = {
+        "Authorization": f"Bearer {twitter_bearer_token}"
+    }
+    return headers
+
+
+# A resource is a function that produces data and yields it
+# Example sources: API endpoint, a database table, a file, or a sheet from a gsheets workbook...
+@dlt.resource(write_disposition="append")
+def twitter_resource(twitter_bearer_token=dlt.secrets.value):
+    headers = _headers(twitter_bearer_token)
+
+    # check if authentication headers look fine
+    print(headers)
+
+    # make an api call here
+    # response = requests.get(url, headers=headers, params=params)
+    # response.raise_for_status()
+    # yield response.json()
+
+    # test data for loading validation, delete it once you yield actual data
+    test_data = [{'id': 0}, {'id': 1}]
+    yield test_data
+
+
+if __name__=='__main__':
+    # print credentials by running the resource
+    data = list(twitter_resource())
+
+    # print responses from resource
+    print(data)
+
+    # run pipeline
+    # configure the pipeline
+    # p = dlt.pipeline(pipeline_name="tester", destination="bigquery", dataset="tester_data")
+    # load_info = p.run(twitter_data(dlt.config.value, dlt.secrets.value, last_id=819273998))
+
+    # pretty print the information on data that was loaded
+    # print(load_info)
+
+
